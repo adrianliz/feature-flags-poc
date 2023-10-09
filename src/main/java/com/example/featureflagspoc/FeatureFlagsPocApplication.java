af3e9d6a36586ed6d41dd3d7185eb7ed7fd06a77
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-09 20:26:32 +0200

    chore: Initial commit

---
+++ src/main/java/com/example/featureflagspoc/FeatureFlagsPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.featureflagspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagsPocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeatureFlagsPocApplication.class, args);
+  }
+
+}
